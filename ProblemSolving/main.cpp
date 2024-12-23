d8c7ba4df2b9587674b19c52d0ca9c3aee43c9c2
Nader Mohamed Nader <nadoorclasher@gmail.com>
2024-12-23 17:09:26 +0200

    Update main.cpp

--- ProblemSolving/main.cpp
+++ ProblemSolving/main.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+
+class Solution {
+public:
+    static int Challenge(const vector<int> &nums, int k) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == k) return mid;
+            if (nums[l] <= nums[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int k = -2;
+    cout << sol.Challenge(nums, k) << endl;
+    return 0;
+}
